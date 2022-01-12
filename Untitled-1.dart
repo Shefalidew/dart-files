764f80609606a56a2b64931fc547019a9ebb019d
Shefalidew <shefali.dew97@gmail.com>
2022-01-12 14:12:04 +0100

    Special constructor added
    By using dot dart feature.

--- Untitled-1.dart
+++ Untitled-1.dart
@@ -4,6 +4,9 @@ class Person{
   Person({@required String inputName, this.age = 30}){
   name = inputName;
   }
+  Person.veryOld(this.name){
+    age = 60;
+  }
 }
 
 double addNumbers(double num1, double num2){
@@ -14,6 +17,9 @@ double addNumbers(double num1, double num2){
 void main(){
   var p1 = Person(inputName: 'Max',age: 30);
   var p2 = Person(age: 31,inputName: 'Manu');
+  var p3 = Person.veryOld('Maddy');
+  print(p3.name);
+  print(p3.age);
   print(p1.name);
   print(p2.name + ' ' p2.age );
   double firstResult;
